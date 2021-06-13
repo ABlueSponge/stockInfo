767f8461ffafdc1449e5fa4a99995c3123ae6691
iamkingg <64109248+iamkingg@users.noreply.github.com>
2021-06-13 11:44:34 -0700

    Add files via upload

---
+++ stockInfo.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+stonk = input("Enter a stock: ")
+stock = yf.Ticker(stonk)
+va = stock.info
+print(stonk.center(20, "="))
+print("Stock Price $"+(str(va["regularMarketPrice"])))
+print("52 Week Low: $" + (str(va["fiftyTwoWeekLow"])))
+print("52 Week High: $" + (str(va["fiftyTwoWeekHigh"])))
+print("50 Day Average: $" + (str(va["fiftyDayAverage"])))
+print("Industry: "+(str(va["industry"])))
+print("Country: " + (str(va["country"])))
+print("Number of Full Time Employees: " + (str(va["fullTimeEmployees"])))
